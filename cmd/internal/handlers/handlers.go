3e7bce887e8aeaa970b605cf27afc4535ed157bb
Ahmad Muslih Zain <ahmad.muslih22@gmail.com>
2022-09-09 09:45:25 +0300

    finish 66

--- cmd/internal/handlers/handlers.go
+++ cmd/internal/handlers/handlers.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/aemzayn/bookings/cmd/internal/config"
+	"github.com/aemzayn/bookings/cmd/internal/forms"
 	"github.com/aemzayn/bookings/cmd/internal/models"
 	"github.com/aemzayn/bookings/cmd/internal/render"
 	"log"
@@ -55,7 +56,14 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 
 // Reservation renders the make a reservation page and displays form
 func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{})
+	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{
+		Form: forms.New(nil),
+	})
+}
+
+// PostReservation handles the posting of a reservation form
+func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
+
 }
 
 // Generals renders the room page
