11b028dcc0d1c4e31cba64afc8c6ec4e66dcc468
Ahmad Muslih Zain <ahmad.muslih22@gmail.com>
2022-09-09 08:11:00 +0300

    finish 38

---
+++ cmd/web/routes.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bookings-udemy/pkg/config"
+	"bookings-udemy/pkg/handlers"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"net/http"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
+	return mux
+}
