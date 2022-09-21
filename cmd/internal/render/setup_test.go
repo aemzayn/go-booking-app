f549faa792ec1c99a4331742bdc462e5bcbb94ad
Ahmad Muslih Zain <ahmad.muslih22@gmail.com>
2022-09-21 09:32:39 +0300

    finish 76

---
+++ cmd/internal/render/setup_test.go
@@ -0,0 +1,36 @@
+package render
+
+import (
+	"encoding/gob"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/aemzayn/bookings/cmd/internal/config"
+	"github.com/aemzayn/bookings/cmd/internal/models"
+	"github.com/alexedwards/scs/v2"
+)
+
+var session *scs.SessionManager
+var testApp config.AppConfig
+
+func TestMain(m *testing.M) {
+	// what am I going to put in the session
+	gob.Register(models.Reservation{})
+	// change this to true when in production
+	testApp.InProduction = false
+
+	// set up the session
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = false
+
+	testApp.Session = session
+
+	app = &testApp
+
+	os.Exit(m.Run())
+}
